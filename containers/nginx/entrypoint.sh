d56e1369ca91d27eb84edba5147d823a6988c830
lireincore <lirein@inbox.ru>
2018-10-28 18:28:00 +0300

    fixes

--- containers/nginx/entrypoint.sh
+++ containers/nginx/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
 envsubst "${ENV_VAR_LIST}" < /etc/nginx/conf.d/app.conf.tmpl > /etc/nginx/conf.d/app.conf
-exec nginx -g 'daemon off;'
+
+exec "$@"
--- containers/php-fpm/healthcheck.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-set -eo pipefail
-
-exit 0
